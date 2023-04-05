3fa81f7b6c21e9bd0cb8de82766716c691db60be
leozinleal <iedpleo@gmail.com>
2023-04-04 21:01:57 -0300

    Aula6

--- sistema.php
+++ sistema.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    include_once('config.php');
     // print_r($_SESSION);
     if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
     {
@@ -8,6 +9,12 @@
         header('Location: login.php');
     }
     $logado = $_SESSION['email'];
+
+    $sql= "SELECT * FROM usuarios ORDER BY id DESC";
+
+    $result = $conexao -> query($sql);
+
+    // print_r($result);
 ?>
 
 <!DOCTYPE html>
@@ -24,6 +31,11 @@
         color: white;
         text-align: center;
         }
+
+        .table-bg{
+            background: rgba(0, 0, 0, 0.3);
+            border-radius: 15px 15px 15px 15px;
+        }
     </style>
 </head>
 <body>
@@ -39,5 +51,50 @@
     <?php
         echo "<h1>Bem vindo <u>$logado</ul></h1>"
     ?>
+    <div class = "m-5">
+        <table class="table text-white table-bg">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Senha</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">Sexo</th>
+                    <th scope="col">Data de Nascimento</th>
+                    <th scope="col">Cidade</th>
+                    <th scope="col">Estado</th>
+                    <th scope="col">Endereço</th>
+                    <th scope="col">...</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                        while($user_data = mysqli_fetch_assoc($result))
+                        {
+                            echo "<tr>";
+                            echo "<td>".$user_data['id']."</td>";
+                            echo "<td>".$user_data['nome']."</td>";
+                            echo "<td>".$user_data['senha']."</td>";
+                            echo "<td>".$user_data['email']."</td>";
+                            echo "<td>".$user_data['telefone']."</td>";
+                            echo "<td>".$user_data['sexo']."</td>";
+                            echo "<td>".$user_data['data_nasc']."</td>";
+                            echo "<td>".$user_data['cidade']."</td>";
+                            echo "<td>".$user_data['estado']."</td>";
+                            echo "<td>".$user_data['endereco']."</td>";
+                            echo "<td>
+                                <a class='btn btn-sm btn-primary' href='edit.php?id=$user_data[id]'>
+                                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+                                <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
+                                </svg>
+                                </a>
+                            </td>";
+                            echo "<tr>";
+                        }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
